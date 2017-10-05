26a875fa6d736108e5916e004233bad411051331
Flávio Losada <flavio_gs96@hotmail.com>
2017-10-05 01:02:01 -0300

    implementacao correlacao e regressao

---
+++ correlacao.m
@@ -0,0 +1,17 @@
+function retorno = correlacao(x, y)
+  mediaX = mean(x);
+  mediaY = mean(y);
+  
+  subX = x - mediaX;
+  subY = y - mediaY;
+  dividendoXY = sum(subX .* subY);
+  
+  powX = sum(subX .^ 2);
+  powY = sum(subY .^ 2);
+  
+  divisorXY = sqrt(powX .* powY);
+  
+  retorno = dividendoXY / divisorXY;
+  
+  %retorno = sum((x-mediaX).*(y - mediaY))/sqrt(sum((x-mediaX).^2) .* sum((y-mediaY).^2));
+end
---
+++ regressao.m
@@ -0,0 +1,14 @@
+function retorno = regressao(x, y)
+  subX = x - mean(x);
+  subY = y - mean(y);
+  
+  sumXY = sum(subX.*subY);
+  divisorXY = sum(subX.^2);
+  
+  b1 = sumXY / divisorXY;
+  
+  b0 = mean(y) - b1.*mean(x);
+  
+  retorno = b0 + b1.*x;
+  
+end
