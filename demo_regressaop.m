cff424065fe750f8b4575fdcd554404afbbbd13a
Matheus Hoeltgebaum <pereiramateduardo@gmail.com>
2017-10-15 17:46:34 -0200

    Reorganizado fase 2 e implementado fase 3 até erro quadrático médio

---
+++ demo_regressaop.m
@@ -0,0 +1,88 @@
+%function retorno = demo_regressaop()
+
+  %a) Carrega os dados
+  file = load('Dados\data_preg.mat');
+  data = file.data;
+  count = size(data)(1);
+  
+  X = ones(count,2);
+  X(:, 2) = data(:, 1);
+  y = data(:, 2);
+  
+  %b) Monta o grafico de dispersao
+  scatter(X(:, 2), y);
+  
+  %c) Traca a linha de regressao (N = 1)
+  %Retorna matriz [valor1(b1) valor2(b0)]
+  reg = polyfit(X(:,2), y, 1);
+  
+  %Calcula a linha de regressao
+  linhaReg1 = polyval(reg, X);
+  
+  hold on;
+  plot(X, linhaReg1, 'r');
+  
+  %d) Traca a linha de regressao (N = 2)
+  %Retorna matriz [valor1(b2) valor2(b1) valor3(b0)]
+  reg = polyfit(X(:,2), y, 2);
+  
+  %Calcula a linha de regressao
+  linhaReg2 = polyval(reg, X);
+  
+  hold on;
+  plot(X, linhaReg2, 'g');
+  
+  %e) Traca a linha de regressao (N = 3)
+  %Retorna matriz [valor1(b3) valor2(b2) valor3(b1) valor4(b0)]
+  reg = polyfit(X(:,2), y, 3);
+  
+  %Calcula a linha de regressao
+  linhaReg3 = polyval(reg, X);
+  
+  hold on;
+  plot(X, linhaReg3, 'k');
+  
+  %f) Traca a linha de regressao (N = 8)
+  reg = polyfit(X(:,2), y, 8);
+  
+  %Calcula a linha de regressao
+  linhaReg8 = polyval(reg, X);
+  
+  hold on;
+  plot(X, linhaReg8, 'y');
+  
+  %g) Calcula EQM (Erro quadratico medio) das linhas de regressao
+  eqmLinha1 = (sum((linhaReg1 - mean(linhaReg1)).^2)) / size(linhaReg1, 1);
+  eqmLinha2 = (sum((linhaReg2 - mean(linhaReg2)).^2)) / size(linhaReg2, 1);
+  eqmLinha3 = (sum((linhaReg3 - mean(linhaReg3)).^2)) / size(linhaReg3, 1);
+  eqmLinha8 = (sum((linhaReg8 - mean(linhaReg8)).^2)) / size(linhaReg8, 1);
+  
+  %h) Montar dados de teste e treinamento
+  %Mistura os dados aleatoriamente
+  %randomData = data(randperm(length(data)));
+  %qtdDataTeste = round(size(randomData)(1) * 0.1);
+  %dataTeste = ones(qtdDataTeste,2);
+  %yTeste = ones(qtdDataTeste);
+  %dataTreinamento = ones(count-qtdDataTeste,2);
+  %yTreinamento = ones(count-qtdDataTeste);
+  
+  
+  %for i=1:qtdDataTeste
+  %  dataTeste(i,2) = randomData(i,1);
+  %  yTeste(i) = randomData(i,2);
+  %end
+  % 
+  %index = 1;
+  %for i=qtdDataTeste+1:size(randomData)(1)
+  %  dataTreinamento(index,2) = randomData(i,1);
+  %  yTreinamento(i) = randomData(i,2);
+  %  index = index + 1;
+  %end
+  
+  %i) Monta linhas de regressao apenas para dados de treinamento
+  
+  %j) Calcula EQM apenas para os dados de teste
+  
+  %k) Método mais preciso
+  
+%end
--- rmdemo.m
+++ rmdemo.m
@@ -1,41 +1,46 @@
 %function retorno = rmdemo()
 
+  %a) Carrega os dados
   file = load('Dados\data.mat');
   data = file.data;
-  sFile = size(data)(1);
-  X = ones(sFile,3);
+  
+  %Quantidade de casas
+  count = size(data)(1);
+  
+  %b) Monta as matrizes
+  X = ones(count,3);
   X(:, 2) = data(:, 1);
   X(:, 3) = data(:, 2);
   y = data(:, 3);
   
-  %disp(X);
-  %return;
+  %c) Verifica a correlacao e regressao
+  %Regressao
+  xReg = X(1:count, 2);
+  tamCasaReg = regressao(xReg, y);
+  xReg = X(1:count, 3);
+  quartoReg = regressao(xReg, y);
+  
+  %Correlacao
+  xCor = X(1:count, 2);
+  tamCasaCor = correlacao(xCor, y);
+  xCor = X(1:count, 3);
+  quartoCor = correlacao(xCor, y);
+  
+  %d) Monta o grafico de dispersao
+  scatter3(X(:, 2), X(:, 3), y);
+  hold on;
+  
+  %Calcula a regressao multipla
   ret = regmultipla(y, X);
   A = X* ret;
-  disp(A);
-
   
-  %c
-  %xReg = X(1:sFile, 2);
-  %tamCasaReg = regressao(xReg, y);
-  
-  %xReg = X(1:sFile, 3);
-  %quartoReg = regressao(xReg, y);
-  
-  %xCor = X(1:sFile, 2);
-  %tamCasaCor = correlacao(xCor, y);
-  
-  %xCor = X(1:sFile, 3);
-  %quartoCor = correlacao(xCor, y);
-  
-  scatter3(X(:, 2), X(:, 3), y);
-  
-  hold on;
+  %e) Traca a linha de regressao
   plot3(X(:, 2), X(:, 3), A);
+  hold on;
   
-  %x = X(1:sFile, 1);
-  %y = X(1:sFile, 2);
-  %z = X(1:sFile, 3);
-  %scatter3(x, y, z);
+  %f) Mostra os coeficientes de correlacao
+  title(["Correlacao tamanho da casa/preco: ", num2str(tamCasaCor), ". Correlacao numero de quartos/preco: ", num2str(quartoCor)]);
+  
+  %g) Calcular o preco da casa com 1650 metros quadrados e 3 quartos
   
 %end
