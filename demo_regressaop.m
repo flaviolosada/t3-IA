df619cc8a9ec215475bb2677c605781880188324
pamelacv <vieira.pamela.c@gmail.com>
2017-10-16 23:03:34 -0200

    ajustes finais

--- demo_regressaop.m
+++ demo_regressaop.m
@@ -85,49 +85,49 @@
   Xtreinamento = ones(size(dataTreinamento),2);
   Xtreinamento(:, 2) = dataTreinamento(:, 2);
   ytreinamento = dataTreinamento(:, 2);
-  reg = polyfit(Xtreinamento(:,2), ytreinamento, 1);
+  regTreinamento = polyfit(Xtreinamento(:,2), ytreinamento, 1);
   
   %Calcula a linha de regressao
-  linhaReg1 = polyval(reg, Xtreinamento);
+  linhaReg1Treinamento = polyval(regTreinamento, Xtreinamento);
   
   hold on;
-  plot(Xtreinamento, linhaReg1, 'b');
+  plot(Xtreinamento, linhaReg1Treinamento, 'b');
   
   %d - Traca a linha de regressao (N = 2)
   %Retorna matriz [valor1(b2) valor2(b1) valor3(b0)]
-  reg = polyfit(Xtreinamento(:,2), ytreinamento, 2);
+  regTreinamento = polyfit(Xtreinamento(:,2), ytreinamento, 2);
   
   %Calcula a linha de regressao
-  linhaReg2 = polyval(reg, Xtreinamento);
+  linhaReg2Treinamento = polyval(regTreinamento, Xtreinamento);
   
   hold on;
-  plot(Xtreinamento, linhaReg2, 'g');
+  plot(Xtreinamento, linhaReg2Treinamento, 'g');
   
   %e - Traca a linha de regressao (N = 3)
   %Retorna matriz [valor1(b3) valor2(b2) valor3(b1) valor4(b0)]
-  reg = polyfit(Xtreinamento(:,2), ytreinamento, 3);
+  regTreinamento = polyfit(Xtreinamento(:,2), ytreinamento, 3);
   
   %Calcula a linha de regressao
-  linhaReg3 = polyval(reg, Xtreinamento);
+  linhaReg3Treinamento = polyval(regTreinamento, Xtreinamento);
   
   hold on;
-  plot(Xtreinamento, linhaReg3, 'k');
+  plot(Xtreinamento, linhaReg3Treinamento, 'k');
   
   %f - Traca a linha de regressao (N = 8)
-  reg = polyfit(Xtreinamento(:,2), ytreinamento, 8);
+  regTreinamento = polyfit(Xtreinamento(:,2), ytreinamento, 8);
   
   %Calcula a linha de regressao
-  linhaReg8 = polyval(reg, Xtreinamento);
+  linhaReg8Treinamento = polyval(regTreinamento, Xtreinamento);
   
   hold on;
-  plot(Xtreinamento, linhaReg8, 'y');
+  plot(Xtreinamento, linhaReg8Treinamento, 'y');
   
   %j) Calcula EQM apenas para os dados de teste
   %g) Calcula EQM (Erro quadratico medio) das linhas de regressao
-  eqmLinha1Teste = (sum((linhaReg1 - mean(linhaReg1)).^2)) / size(linhaReg1, 1);
-  eqmLinha2Teste = (sum((linhaReg2 - mean(linhaReg2)).^2)) / size(linhaReg2, 1);
-  eqmLinha3Teste = (sum((linhaReg3 - mean(linhaReg3)).^2)) / size(linhaReg3, 1);
-  eqmLinha8Teste = (sum((linhaReg8 - mean(linhaReg8)).^2)) / size(linhaReg8, 1);
+  eqmLinha1Teste = (sum((linhaReg1Treinamento - mean(linhaReg1Treinamento)).^2)) / size(linhaReg1Treinamento, 1);
+  eqmLinha2Teste = (sum((linhaReg2Treinamento - mean(linhaReg2Treinamento)).^2)) / size(linhaReg2Treinamento, 1);
+  eqmLinha3Teste = (sum((linhaReg3Treinamento - mean(linhaReg3Treinamento)).^2)) / size(linhaReg3Treinamento, 1);
+  eqmLinha8Teste = (sum((linhaReg8Treinamento - mean(linhaReg8Treinamento)).^2)) / size(linhaReg8Treinamento, 1);
   
   %k) Método mais preciso
   
